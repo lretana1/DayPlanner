2abf4b4b72e16dd439096b14d843873f5658be63
LuisRetana <luisretana123@gmail.com>
2021-07-18 22:30:47 -0400

    added logic if time block is current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,23 @@
 var today = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(today);
 
-function(){
-    var currtime =moment().hour();
-    $(".time-block").each(function(){
-        var timeblock =parseint($(this).attr("id").split("hour")[1]);
-    })
-}
 
 $(document).ready(function(){
     $(".saveBtn").on("click",function(){
         var text = $(this).siblings(".desc").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time,text);
+    }
+    function tracker(){
+        var now =moment().hour();
+        $(".time-block").each(function{
+            var timeblock = parseInt($(this).attr("id").split("hour")[1]);
+             if (timeblock=== now){
+                 $(this).removeClass("future");
+                 $(this).removeClass("past");
+                 $(this).removeClass("present");
+             }
+             else if(timeblock)
+        })
     })
 })
